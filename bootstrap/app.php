2e748123e08e7fba1d814fade121bd307322cc43
Taha <tahacollage@gmail.com>
2026-01-09 17:22:24 +0200

    start

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
+        $middleware->trustProxies(at: '*');
+
 
         //
     })
